22bf791cc8bf39e7e9aa2b85e91500a4176ad699
ShamWerks <sham@free.fr>
2019-11-29 16:57:56 +0100

    updating version to 0.2

--- src/main/java/com/shamwerks/camwerks/config/Config.java
+++ src/main/java/com/shamwerks/camwerks/config/Config.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class Config {
 
     public static final String CONFIG_FILE = "config/config.properties";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     
     private String language;
     private int nbSteps;
